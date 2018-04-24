eeb9f1551a72318b9d1a83383726c613bf0ac9ae
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-04-24 11:59:46 +0200

    Simplify MapMenu

--- src/components/MapMenu.js
+++ src/components/MapMenu.js
@@ -4,18 +4,21 @@ import { ORTHO_LAYERS_IDS } from '../settings/layers';
 
 class MapMenu extends Component {
   render () {
+    const { showMaps, selection } = this.props;
     return (
       <div>
-        <select value={this.props.selection.join('-')} onChange={event => this.props.showMaps(...event.target.value.split('-'))}>
-          {['Aucun', ...ORTHO_LAYERS_IDS].map(layerID =>
+        <select value={selection.join('-')} onChange={event => showMaps(...event.target.value.split('-'))}>
+          <option>------------</option>
+          <option value="2012">2012 uniquement</option>
+          {ORTHO_LAYERS_IDS.map(layerID =>
             <option key={layerID} value={[2012, layerID].join('-')}>Compare with {layerID}</option>
           )}
         </select>
 
         <ul>
-          <li><button onClick={() => this.props.showMaps('1850')}>Cartes 1850</button></li>
-          <li><button onClick={() => this.props.showMaps('1850')}>Cartes Cassini</button></li>
-          <li><button onClick={() => this.props.showMaps('1850')}>Cadastre Napoléonien</button></li>
+          <li><button onClick={() => showMaps('1850')}>Cartes 1850</button></li>
+          <li><button onClick={() => showMaps('cassini')}>Cartes Cassini</button></li>
+          <li><button onClick={() => showMaps('napoleon')}>Cadastre Napoléonien</button></li>
         </ul>
       </div>
     );
