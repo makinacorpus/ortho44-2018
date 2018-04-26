be54c5e195b4e02a468298d2fde1a0092701635e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-04-26 11:07:30 +0200

    Convert MapMenu functional component to class component

--- src/components/MapMenu.js
+++ src/components/MapMenu.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { DEFAULT_BASE, COMPARE_WITH, ALL_LAYERS } from '../settings/layers';
 import GeoSearch from './GeoSearch';
 
-const MapMenu = props => {
-  const { showMaps, selection, className } = props;
-  return (
-    <div className={className}>
+class MapMenu extends Component {
+  constructor () {
+    super();
+  }
 
-      <GeoSearch
-        onSelect={props.handleResult}
-        inputProps={{
-          placeholder: 'exemple : 3 quai Ceineray, Nantes',
-        }}
-      />
+  render () {
+    const { props } = this;
+    const { showMaps, selection, className } = props;
+    return (
+      <div className={className}>
 
-      <select
-        value={selection.join('-')}
-        onChange={event => showMaps(...event.target.value.split('-'))}>
-        <option disabled>-</option>
-        <option value={DEFAULT_BASE}>Aucun</option>
-        {COMPARE_WITH.map(layerID => (
-          <option
-            key={layerID}
-            value={[DEFAULT_BASE, layerID].join('-')}
-          >{ALL_LAYERS[layerID].label || layerID}</option>
-        ))}
-      </select>
+        <GeoSearch
+          onSelect={props.handleResult}
+          inputProps={{
+            placeholder: 'exemple : 3 quai Ceineray, Nantes',
+          }}
+        />
 
-      <ul>
-        <li><button onClick={() => showMaps('1850')}>Cartes 1850</button></li>
-        <li><button onClick={() => showMaps('cassini')}>Cartes Cassini</button></li>
-        <li><button onClick={() => showMaps('napoleon')}>Cadastre Napoléonien</button></li>
-      </ul>
-    </div>
-  );
-};
+        <select
+          value={selection.join('-')}
+          onChange={event => showMaps(...event.target.value.split('-'))}>
+          <option disabled>-</option>
+          <option value={DEFAULT_BASE}>Aucun</option>
+          {COMPARE_WITH.map(layerID => (
+            <option
+              key={layerID}
+              value={[DEFAULT_BASE, layerID].join('-')}
+            >{ALL_LAYERS[layerID].label || layerID}</option>
+          ))}
+        </select>
+
+        <ul>
+          <li><button onClick={() => showMaps('1850')}>Cartes 1850</button></li>
+          <li><button onClick={() => showMaps('cassini')}>Cartes Cassini</button></li>
+          <li><button onClick={() => showMaps('napoleon')}>Cadastre Napoléonien</button></li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default MapMenu;
