cb1588f64891e07bb0406f724de2934577c1469d
Flo <floriansommariva@gmail.com>
2018-05-30 11:33:57 +0200

    Style POI permalink page

--- src/templates/poi.js
+++ src/templates/poi.js
@@ -1,41 +1,24 @@
 import React from 'react';
-import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
 import Content, { HTMLContent } from '../components/Content';
 
 export const PoiPageTemplate = ({
   content,
   contentComponent,
   description,
-  tags,
   title,
   helmet,
 }) => {
-  const PostContent = contentComponent || Content;
+  const PageContent = contentComponent || Content;
 
   return (
-    <section>
+    <section className="u-site__content">
       {helmet || ''}
-      <div>
-        <h1>
-          {title}
-        </h1>
-        <p>{description}</p>
-        <PostContent content={content} />
-        {tags && tags.length ? (
-          <div style={{ marginTop: '4rem' }}>
-            <h4>Tags</h4>
-            <ul>
-              {tags.map(tag => (
-                <li key={`${tag}tag`}>
-                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : null}
-      </div>
+      <h1 className="u-site__title">
+        {title}
+      </h1>
+      <p className="u-site__excerpt">{description}</p>
+      <PageContent className="t-md" content={content} />
     </section>
   );
 };
@@ -48,7 +31,7 @@ const PoiPage = ({ data }) => {
       content={post.html}
       contentComponent={HTMLContent}
       description={post.frontmatter.description}
-      helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+      helmet={<Helmet title={`${post.frontmatter.title} | Point d'intérêt`} />}
       tags={post.frontmatter.tags}
       title={post.frontmatter.title}
     />
