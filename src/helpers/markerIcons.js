836dea7f31e81dde6a7674495f0d4d91c520e9f4
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-06-08 19:09:57 +0200

    Generate leaflet marker icons from colors

--- src/helpers/markerIcons.js
+++ src/helpers/markerIcons.js
@@ -4,13 +4,37 @@ import markerUrl from '../img/marker-icon.png';
 import markerShadowUrl from '../img/marker-shadow.png';
 import markerRetinaUrl from '../img/marker-icon-2x.png';
 
-const icons = {
+const inRealBrowser = typeof window !== 'undefined';
+
+const colorIconsUrl = inRealBrowser && [
+  'black',
+  'blue',
+  'green',
+  'grey',
+  'orange',
+  'red',
+  'violet',
+  'yellow',
+].reduce((acc, color) => ({
+  ...acc,
+  [color]: new L.Icon({
+    ...L.Icon.Default.prototype.options,
+    /* eslint-disable import/no-dynamic-require, global-require */
+    iconUrl: require(`../img/marker-icon-${color}.png`),
+    iconRetinaUrl: require(`../img/marker-icon-2x-${color}.png`),
+    /* eslint-enable */
+    shadowUrl: markerShadowUrl,
+  }),
+}), {});
+
+const markerIcons = inRealBrowser && {
   default: new L.Icon({
     ...L.Icon.Default.prototype.options,
     iconUrl: markerUrl,
     shadowUrl: markerShadowUrl,
     iconRetinaUrl: markerRetinaUrl,
   }),
+  ...colorIconsUrl,
 };
 
-export default icons;
+export default markerIcons;
