makinacorpus/ortho44-2018

commit 4ccf7672ea01771c0eed86e5cf7f00df1a8100c7

Do a lot of linting corrections

src/components/GeoSearch.js
@@ -7,26 +7,37 @@ import { parseSuggestions } from '../helpers';
 
 import './GeoSearch.scss';
 
-const renderSuggestion = (suggestion, { isHighlighted }) => {
-  return (
-    <span>{suggestion.label}</span>
-  );
-};
+const renderSuggestion = suggestion => <span>{suggestion.label}</span>;
 
 class GeoSearch extends Component {
-  constructor() {
+  constructor () {
     super();
 
     this.state = {
       value: '',
-      suggestions: []
+      isLoading: false,
+      suggestions: [],
     };
 
     this.loadSuggestions = debounce(this.loadSuggestions, 500);
   }
 
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
 
-  loadSuggestions(value) {
+  onSuggestionSelected = (event, selection) => {
+    // const { suggestion, suggestionValue, suggestionIndex, sectionIndex, method } = selection;
+    if (typeof this.props.onSelect === 'function') {
+      this.props.onSelect(selection);
+    }
+  };
+
+  getSuggestionValue = suggestion => suggestion.label;
+
+  loadSuggestions (value) {
     this.setState({
       isLoading: true,
     });
@@ -47,12 +58,12 @@ class GeoSearch extends Component {
         if (data.hits.total === 0) {
           fetch(`https://es.makina-corpus.net/cg44/address/_search?default_operator=AND&q=${value}`)
             .then(res => res.json())
-            .then(data => {
+            .then(data2 => {
               this.setState({
                 isLoading: false,
-                suggestions: parseSuggestions(data.hits.hits),
+                suggestions: parseSuggestions(data2.hits.hits),
               });
-            })
+            });
         } else {
           this.setState({
             isLoading: false,
@@ -62,36 +73,19 @@ class GeoSearch extends Component {
       });
   }
 
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
-  };
-
-  getSuggestionValue = suggestion => {
-    return suggestion.label;
-  };
-
-  onSuggestionSelected = (event, selection) => {
-    // const { suggestion, suggestionValue, suggestionIndex, sectionIndex, method } = selection;
-    if (typeof this.props.onSelect === 'function') {
-      this.props.onSelect(selection);
-    }
-  };
-
-  render() {
-    const { value, suggestions, isLoading } = this.state;
+  render () {
+    const { value, suggestions } = this.state;
     const inputProps = {
       ...this.props.inputProps,
       value,
-      onChange: this.onChange
+      onChange: this.onChange,
     };
 
     return (
       <div>
         <Autosuggest
           suggestions={suggestions}
-          onSuggestionsFetchRequested={({ value }) => this.loadSuggestions(value)}
+          onSuggestionsFetchRequested={({ val }) => this.loadSuggestions(val)}
           onSuggestionsClearRequested={() => this.setState({ suggestions: [] })}
           onSuggestionSelected={this.onSuggestionSelected}
           getSuggestionValue={this.getSuggestionValue}

src/components/CarouselPOI.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'gatsby-link';
 import Carousel from 'nuka-carousel';
 

src/components/MapActions.js
@@ -22,8 +22,8 @@ class MapActions extends Component {
     this.setState({ displayRoadsAndBoundaries: false });
   }
 
-  render() {
-    const { 
+  render () {
+    const {
       className,
       geolocate,
       roads,
@@ -33,7 +33,7 @@ class MapActions extends Component {
       fullscreen,
       toggleFullscreen,
       zoomIn,
-      zoomOut
+      zoomOut,
     } = this.props;
     const { displayRoadsAndBoundaries } = this.state;
     return (
@@ -44,7 +44,7 @@ class MapActions extends Component {
             <span className="u-visually-hidden">Géolocaliser</span>
           </button>
         </li>
-        
+
         <li className={`${className}__item`}>
           { fullscreen ? (
             <button className={`${className}__button`} type="button" onClick={() => toggleFullscreen()} title="Enlever le plein écran">
@@ -94,9 +94,9 @@ class MapActions extends Component {
           </ul>
         </li>
       </ul>
-    )
+    );
   }
-};
+}
 
 export default MapActions;
 

src/components/MapMenu.js
@@ -20,7 +20,16 @@ class MapMenu extends Component {
   }
 
   render () {
-    const { showMaps, selection, toggleCadastre, toggleDlNotice, cadastre, className, handleResult, placeName } = this.props;
+    const {
+      showMaps,
+      selection,
+      toggleCadastre,
+      toggleDlNotice,
+      cadastre,
+      className,
+      handleResult,
+      placeName,
+    } = this.props;
     const { open } = this.state;
 
     const displayPrintSelection = selection.length === 1 ? `Carte ${selection[0]} ${cadastre ? 'avec affichage du cadastre' : ''}` : `À gauche: carte ${selection[0]} ${cadastre ? 'avec affichage du cadastre' : ''}; À droite: carte ${selection[1]}`;

src/helpers/index.js
@@ -25,12 +25,11 @@ export const getLabel = (hit, mode) => {
   return label;
 };
 
-export const parseSuggestions = hits => {
-  return hits.map(hit => ({
-    label: getLabel(hit._source, 'LISTING'),
+export const parseSuggestions = hits =>
+  hits.map(hit => ({
+    label: getLabel(hit._source, 'LISTING'), // eslint-disable-line no-underscore-dangle
     data: hit,
   }));
-};
 
 export const getRandomPlace = () => {
   const places = [

src/layouts/index.js
@@ -22,13 +22,11 @@ export default class TemplateWrapper extends React.Component {
   static childContextTypes = {
     setPosts: PropTypes.func,
   }
-  getChildContext = () => {
-    return {
-      setPosts: posts => {
-        this.posts = posts;
-      },
-    }
-  }
+  getChildContext = () => ({
+    setPosts: posts => {
+      this.posts = posts;
+    },
+  })
 
   componentDidMount = () => {
     // Create references to html/body elements
@@ -39,7 +37,7 @@ export default class TemplateWrapper extends React.Component {
     this.windowWidth = window.innerWidth;
   }
 
-  componentWillReceiveProps = (nextProps) => {
+  componentWillReceiveProps = nextProps => {
     // if we're changing to a non-homepage page, put things in
     // a modal (unless we're on mobile).
     if (

src/pages/index.js
@@ -262,12 +262,12 @@ export default class IndexPage extends React.Component {
                 ?
                   [
                     <li key="ecw">
-                      <a href={this.getECWUrl()} target="_blank">
+                      <a href={this.getECWUrl()} target="_blank" rel="noopener noreferrer">
                         Télécharger l'image en dalles ECW
                       </a>
                     </li>,
                     <li key="jpg">
-                      <a href={this.getWMSPictureUrl()} target="_blank">
+                      <a href={this.getWMSPictureUrl()} target="_blank" rel="noopener noreferrer">
                         Télécharger l'image haute résolution JPG
                       </a>
                     </li>,

src/templates/tags.js
@@ -2,37 +2,35 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 
-class TagRoute extends React.Component {
-  render () {
-    const posts = this.props.data.allMarkdownRemark.edges;
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2>{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ));
-    const { tag, title, totalCount } = this.props.pathContext;
-    const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`;
+const TagRoute = props => {
+  const posts = props.data.allMarkdownRemark.edges;
+  const postLinks = posts.map(post => (
+    <li key={post.node.fields.slug}>
+      <Link to={post.node.fields.slug}>
+        <h2>{post.node.frontmatter.title}</h2>
+      </Link>
+    </li>
+  ));
+  const { tag, title, totalCount } = props.pathContext;
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? '' : 's'
+  } tagged with “${tag}”`;
 
-    return (
-      <section>
-        <Helmet title={`${tag} | ${title}`} />
-          <div
-            style={{ marginBottom: '6rem' }}
-          >
-            <h3>{tagHeader}</h3>
-            <ul>{postLinks}</ul>
-            <p>
-              <Link to="/tags/">Browse all tags</Link>
-            </p>
-          </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section>
+      <Helmet title={`${tag} | ${title}`} />
+      <div
+        style={{ marginBottom: '6rem' }}
+      >
+        <h3>{tagHeader}</h3>
+        <ul>{postLinks}</ul>
+        <p>
+          <Link to="/tags/">Browse all tags</Link>
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default TagRoute;
 

commit 3a42a485e45e5c67dbec53e3ed314fe66262919b

Fix geoLookup broken after linting
	Broken by 4ccf7672ea01771c0eed86e5cf7f00df1a8100c7

src/components/GeoSearch.js
@@ -91,7 +91,7 @@ class GeoSearch extends Component {
       <div>
         <Autosuggest
           suggestions={suggestions}
-          onSuggestionsFetchRequested={({ val }) => this.loadSuggestions(val)}
+          onSuggestionsFetchRequested={res => this.loadSuggestions(res.value)}
           onSuggestionsClearRequested={() => this.setState({ suggestions: [] })}
           onSuggestionSelected={this.onSuggestionSelected}
           getSuggestionValue={this.getSuggestionValue}