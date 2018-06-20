2b7666f924df2a37199871bb3a1102f586d14614
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-06-20 10:42:03 +0200

    Push Carousel custom components to a dedicate file

--- src/components/CarouselPOI.js
+++ src/components/CarouselPOI.js
@@ -1,64 +1,13 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import Carousel from 'nuka-carousel';
-
-import Icon from './Icon';
-import mediaTypes from '../helpers/mediaTypes';
+import {
+  CustomCarouselPrev,
+  CustomCarouselNext,
+  CustomCarouselItemList,
+  CustomCarouselSlide,
+} from './CarouselPOIElements';
 
 import './CarouselPOI.scss';
-
-const iArray = count => Array.from(Array(count).keys());
-
-const CustomCarouselButton = ({ onClick, className, text }) => (
-  <button className={className} onClick={onClick}>
-    <Icon name="arrow-top" />
-    <span className="u-visually-hidden">{text}</span>
-  </button>
-);
-
-const CustomCarouselPrev = ({ onClick, classNamePrefix }) => (
-  <CustomCarouselButton
-    onClick={onClick}
-    className={`${classNamePrefix}__prev`}
-    text="Précédent"
-  />
-);
-
-const CustomCarouselNext = ({ onClick, classNamePrefix }) => (
-  <CustomCarouselButton
-    onClick={onClick}
-    className={`${classNamePrefix}__next`}
-    text="Suivant"
-  />
-);
-
-const CustomCarouselItemList = ({ classNamePrefix, slideCount, currentSlide, goToSlide }) => (
-  <div className={`${classNamePrefix}__control-list`}>
-    {iArray(slideCount).map(slide => (
-      <button
-        className={`${classNamePrefix}__control`}
-        key={slide}
-        onClick={() => goToSlide(slide)}
-        data-active={currentSlide === slide}
-      >
-        {slide + 1}
-      </button>
-    ))}
-  </div>
-);
-
-const CustomCarouselSlide = ({ node, classNamePrefix }) => (
-  <div className={`${classNamePrefix}__item`}>
-    <img className={`${classNamePrefix}__img`} src={node.frontmatter.picture} alt="" />
-    <img className={`${classNamePrefix}__type-img`} src={mediaTypes[node.frontmatter.media_type]} alt={node.frontmatter.media_type} />
-    <div className={`${classNamePrefix}__content`}>
-      <h2 className={`${classNamePrefix}__item-title`}>{node.frontmatter.title}</h2>
-      <Link className={`${classNamePrefix}__button`} to={node.fields.slug}>
-        Découvrir
-      </Link>
-    </div>
-  </div>
-);
 
 const CarouselPOI = props => {
   const { className, posts, headerContent } = props;
