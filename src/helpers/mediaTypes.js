02a75a3feccdf9fb0356ffb486f660a3122d8583
Flo <floriansommariva@gmail.com>
2018-07-03 15:18:17 +0200

    Add Icons for all mediaTypes

--- src/helpers/mediaTypes.js
+++ src/helpers/mediaTypes.js
@@ -1,9 +1,9 @@
 const mediaTypes = [
   'drone',
-  // 'photo',
-  // 'photos',
-  // 'video',
-  // '360',
+  'photo',
+  'photos',
+  'video',
+  '360',
 ].reduce((acc, id) => ({
   ...acc,
   [id]: require(`../img/media-${id}.png`), // eslint-disable-line import/no-dynamic-require, global-require
