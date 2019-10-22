400ad8d964a111faceafe85595bb08badf3ac4a5
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-10-22 12:20:06 +0200

    Send analytics pageView only when path changed

--- plugins/gatsby-plugin-google-analytics/gatsby-browser.js
+++ plugins/gatsby-plugin-google-analytics/gatsby-browser.js
@@ -2,16 +2,20 @@
 
 exports.onRouteUpdate = function (_ref) {
   var location = _ref.location;
-  const isPush = _ref.action === 'PUSH';
 
   // Don't track while developing.
-  if (isPush && process.env.NODE_ENV === "production" && typeof ga === "function") {
+  if (process.env.NODE_ENV === "production" && typeof ga === "function") {
     if (location && typeof window.excludeGAPaths !== "undefined" && window.excludeGAPaths.some(function (rx) {
       return rx.test(location.pathname);
     })) {
       return;
     }
     window.ga("set", "page", location ? location.pathname + location.search + location.hash : undefined);
-    window.ga("send", "pageview");
+
+    const path = location ? location.pathname + location.search : undefined;
+    if (path !== window.prevPath) {
+      window.ga("send", "pageview");
+    }
+    window.prevPath = path;
   }
 };
