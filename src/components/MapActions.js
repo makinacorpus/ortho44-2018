a9c27b7975b8f9b20783ed3bbe2c87f438b33dc5
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-05-10 19:10:50 +0200

    Convert MapActions to function component

--- src/components/MapActions.js
+++ src/components/MapActions.js
@@ -1,102 +1,86 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import Icon from './Icon';
 
 import './MapActions.scss';
 
 
-class MapActions extends Component {
-  constructor () {
-    super();
+const MapActions = ({
+  className,
+  geolocate,
+  roads,
+  toggleRoads,
+  boundaries,
+  toggleBoundaries,
+  fullscreen,
+  toggleFullscreen,
+  zoomIn,
+  zoomOut,
+}) => {
+  const [displayRoadsAndBoundaries, setDisplayRoadsAndBoundaries] = React.useState(false);
+  const handleMouseEnter = () => setDisplayRoadsAndBoundaries(true);
+  const handleMouseLeave = () => setDisplayRoadsAndBoundaries(false);
 
-    this.state = {
-      displayRoadsAndBoundaries: false,
-    };
-  }
+  return (
+    <ul className={className}>
+      <li className={`${className}__item`}>
+        <button className={`${className}__button`} type="button" onClick={() => geolocate()} title="Géolocaliser">
+          <Icon name="gps" />
+          <span className="u-visually-hidden">Géolocaliser</span>
+        </button>
+      </li>
 
-  onMouseEnter = () => {
-    this.setState({ displayRoadsAndBoundaries: true });
-  }
-
-  onMouseLeave = () => {
-    this.setState({ displayRoadsAndBoundaries: false });
-  }
-
-  render () {
-    const {
-      className,
-      geolocate,
-      roads,
-      toggleRoads,
-      boundaries,
-      toggleBoundaries,
-      fullscreen,
-      toggleFullscreen,
-      zoomIn,
-      zoomOut,
-    } = this.props;
-    const { displayRoadsAndBoundaries } = this.state;
-    return (
-      <ul className={className}>
-        <li className={`${className}__item`}>
-          <button className={`${className}__button`} type="button" onClick={() => geolocate()} title="Géolocaliser">
-            <Icon name="gps" />
-            <span className="u-visually-hidden">Géolocaliser</span>
+      <li className={`${className}__item`}>
+        { fullscreen ? (
+          <button className={`${className}__button`} type="button" onClick={() => toggleFullscreen()} title="Enlever le plein écran">
+            <Icon name="fullscreen-out" />
+            <span className="u-visually-hidden">Enlever plein écran</span>
           </button>
-        </li>
-
-        <li className={`${className}__item`}>
-          { fullscreen ? (
-            <button className={`${className}__button`} type="button" onClick={() => toggleFullscreen()} title="Enlever le plein écran">
-              <Icon name="fullscreen-out" />
-              <span className="u-visually-hidden">Enlever plein écran</span>
-            </button>
-          ) : (
-            <button className={`${className}__button`} type="button" onClick={() => toggleFullscreen()} title="Mettre en plein écran">
-              <Icon name="fullscreen" />
-              <span className="u-visually-hidden">Mettre en plein écran</span>
-            </button>
-          )}
-        </li>
-        <li className={`${className}__item`}>
-          <button className={`${className}__button`} type="button" onClick={() => zoomIn()} title="Zoomer">
-            <Icon name="zoom-in" />
-            <span className="u-visually-hidden">Zoomer</span>
+        ) : (
+          <button className={`${className}__button`} type="button" onClick={() => toggleFullscreen()} title="Mettre en plein écran">
+            <Icon name="fullscreen" />
+            <span className="u-visually-hidden">Mettre en plein écran</span>
           </button>
-        </li>
-        <li className={`${className}__item`}>
-          <button className={`${className}__button`} type="button" onClick={() => zoomOut()} title="Dézoomer">
-            <Icon name="zoom-out" />
-            <span className="u-visually-hidden">Dézoomer</span>
-          </button>
-        </li>
-        <li className={`${className}__item`}>
-          <button
-            className={`${className}__button`}
-            type="button"
-            title="Afficher les limites départementales et routes"
-            onMouseEnter={this.onMouseEnter}
-          >
-            <Icon name="map" />
-          </button>
-          <ul
-            className={classnames(`${className}__sub`, { 'is-expanded': displayRoadsAndBoundaries })}
-            onMouseLeave={this.onMouseLeave}
-          >
-            <li className={`${className}__sub-item`}>
-              <input id={`${className}__sub-boudaries`} className={`${className}__sub-input`} type="checkbox" checked={boundaries} onChange={() => toggleBoundaries()} />
-              <label htmlFor={`${className}__sub-boudaries`} className={`${className}__sub-label`}>Afficher les limites départementales</label>
-            </li>
-            <li className={`${className}__sub-item`}>
-              <input id={`${className}__sub-roads`} className={`${className}__sub-input`} type="checkbox" checked={roads} onChange={() => toggleRoads()} />
-              <label htmlFor={`${className}__sub-roads`} className={`${className}__sub-label`}>Afficher les rues</label>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    );
-  }
-}
+        )}
+      </li>
+      <li className={`${className}__item`}>
+        <button className={`${className}__button`} type="button" onClick={() => zoomIn()} title="Zoomer">
+          <Icon name="zoom-in" />
+          <span className="u-visually-hidden">Zoomer</span>
+        </button>
+      </li>
+      <li className={`${className}__item`}>
+        <button className={`${className}__button`} type="button" onClick={() => zoomOut()} title="Dézoomer">
+          <Icon name="zoom-out" />
+          <span className="u-visually-hidden">Dézoomer</span>
+        </button>
+      </li>
+      <li className={`${className}__item`}>
+        <button
+          className={`${className}__button`}
+          type="button"
+          title="Afficher les limites départementales et routes"
+          onMouseEnter={handleMouseEnter}
+        >
+          <Icon name="map" />
+        </button>
+        <ul
+          className={classnames(`${className}__sub`, { 'is-expanded': displayRoadsAndBoundaries })}
+          onMouseLeave={handleMouseLeave}
+        >
+          <li className={`${className}__sub-item`}>
+            <input id={`${className}__sub-boudaries`} className={`${className}__sub-input`} type="checkbox" checked={boundaries} onChange={() => toggleBoundaries()} />
+            <label htmlFor={`${className}__sub-boudaries`} className={`${className}__sub-label`}>Afficher les limites départementales</label>
+          </li>
+          <li className={`${className}__sub-item`}>
+            <input id={`${className}__sub-roads`} className={`${className}__sub-input`} type="checkbox" checked={roads} onChange={() => toggleRoads()} />
+            <label htmlFor={`${className}__sub-roads`} className={`${className}__sub-label`}>Afficher les rues</label>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
+};
 
 export default MapActions;
 
